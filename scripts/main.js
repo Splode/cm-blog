5946eddc7eae6362c6e75f0623d5c415d427bcb8
Christopher Murphy <christopherianmurphy@gmail.com>
2017-05-18 22:46:27 -0600

    Search Functionality
    Implementing a post search function using JSON output from Jekyll for
    each post and filtering with Vue.js.

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,28 @@ $(document).ready(function() {
   // Mobile menu icon toggle
   $(".menu-icon").click(function() {
     $(".menu").slideToggle("slow");
-    console.log("works");
   });
+
+  // console.log(posts);
+});
+
+new Vue({
+  el: '#app-container',
+  data: {
+    searchItem: '',
+    posts: posts,
+  },
+  computed: {
+    filterSearch: function() {
+      var vm = this;
+      return this.posts.filter(function(element) {
+        return element.title.toLowerCase().match(vm.searchItem.toLowerCase());
+      });
+    },
+  },
+  created: function() {
+    console.log('vue initiated');
+    console.log(posts);
+    console.log(this.filterSearch);
+  }
 });
