8b8c594ab103751e2d03e08f774b5163e0e687a5
Christopher Murphy <christopherianmurphy@gmail.com>
2017-05-27 16:14:02 -0600

    Expand / Collapse Navbar
    Succesfully implemented an expanding/collapsing navbar with Vue.js
    integration.

--- _drafts/expand-collapse-navbar.md
+++ _drafts/expand-collapse-navbar.md
@@ -6,18 +6,24 @@ date: 2017-05-16
 author: Christopher Murphy
 description: A simple tutorial for creating a navigation menu bar that expands and collapses when a user scrolls the page.
 image: /assets/images/posts/007_expand-collapse-menu/007_expand-collapse-menu.gif
+categories: JavaScript navigation UI/UX Vue.js
 ---
 
 ## What we'll be making
-Here is a thing.
+Screen real estate is a precious commodity, especially on small displays, such as mobile devices. One strategy to maximize content display is to dynamically control the display of the primary navigation depending on whether or not the user is scrolling up or scrolling down. It's quite simple: scroll down on the page and the navigation recedes; scroll up on the page and the navigation reappears.
+
+Implementing this strategy is quite simple. We'll be creating the following example using native JavaScript and then integrating with a Vue.js instance.
 
 ### Base Example
-Here's an basic example of the expanding/collapsing nav-bar that we'll be making.
+Here's an basic example (with Vue.js integration) of the expanding/collapsing nav-bar that we'll be making.
 
 <p data-height="495" data-theme-id="0" data-slug-hash="mmeQzd" data-default-tab="result" data-user="Splode" data-embed-version="2" data-pen-title="Expand/Collapse Navigation Menu - Baseline" class="codepen">See the Pen <a href="http://codepen.io/Splode/pen/mmeQzd/">Expand/Collapse Navigation Menu - Baseline</a> by Christopher Murphy (<a href="http://codepen.io/Splode">@Splode</a>) on <a href="http://codepen.io">CodePen</a>.</p>
 <script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
 
 <figcaption>Scroll to see the nav-bar expanding and collapsing effects.</figcaption>
+
+### Detecting Scroll Actions
+We'll start by creating a function that returns the position of the browser window along the Y-axis. When called, this function will return the value, in pixels, of the uppermost portion of the window (the point at which content is clipped on scroll). `scrollY` is a native property of the browser window object.
 
 {% highlight JavaScript %}
 // returns current scroll position
--- scripts/main.js
+++ scripts/main.js
@@ -7,38 +7,89 @@ $(document).ready(function() {
   // console.log(posts);
 });
 
+// new Vue({
+//   el: '#app-container',
+//   data: {
+//     searchObj: {
+//       searchItem: '',
+//       open: false,
+//     },
+//     posts: posts,
+//   },
+//   computed: {
+//     filterSearch: function() {
+//       var vm = this;
+//       return this.posts.filter(function(element) {
+//         return element.title.toLowerCase().match(vm.searchObj.searchItem.toLowerCase());
+//       });
+//     },
+//   },
+//   methods: {
+//     searchClose: function() {
+//       this.searchToggle();
+//       this.searchClear();
+//     },
+//     searchClear: function() {
+//       this.searchObj.searchItem = '';
+//     },
+//     searchToggle: function() {
+//       this.searchObj.open = !this.searchObj.open;
+//     },
+//   },
+//   created: function() {
+//     console.log('vue initiated');
+//     console.log(posts);
+//     console.log(this.filterSearch);
+//   }
+// });
+
+// Scroll Collapse / Expand
 new Vue({
   el: '#app-container',
   data: {
-    searchObj: {
-      searchItem: '',
+    // Classes assigned to nav on scroll
+    navBar: {
+      collapse: false,
       open: false,
     },
-    posts: posts,
-  },
-  computed: {
-    filterSearch: function() {
-      var vm = this;
-      return this.posts.filter(function(element) {
-        return element.title.toLowerCase().match(vm.searchObj.searchItem.toLowerCase());
-      });
-    },
+    scrollState: 0, // Used to keep track of scroll position
   },
   methods: {
-    searchClose: function() {
-      this.searchToggle();
-      this.searchClear();
+    scrollDetect: function (home, down, up) {
+      // Current scroll position
+      var currentScroll = this.scrollTop();
+      if (this.scrollTop() === 0) {
+        home();
+      } else if (currentScroll > (this.scrollState)) {
+        down();
+      } else {
+        up();
+      }
+      // Set previous scroll position
+      this.scrollState = this.scrollTop();
     },
-    searchClear: function() {
-      this.searchObj.searchItem = '';
+    // Returns current scroll position
+    scrollTop: function () {
+      return window.scrollY;
     },
-    searchToggle: function() {
-      this.searchObj.open = !this.searchObj.open;
+    // Called when scroll is in initial position
+    scrollHome: function () {
+    },
+    // Called when scrolled down
+    scrollDown: function () {
+      this.navBar.collapse = true;
+      this.navBar.open = false;
+    },
+    // Called when scolled up
+    scrollUp: function () {
+      this.navBar.collapse = false;
+      this.navBar.open = true;
     },
   },
-  created: function() {
-    console.log('vue initiated');
-    console.log(posts);
-    console.log(this.filterSearch);
+  created: function () {
+    var vm = this;
+    window.addEventListener('scroll', function() {
+      vm.scrollDetect(vm.scrollHome, vm.scrollDown, vm.scrollUp);
+    });
   }
 });
